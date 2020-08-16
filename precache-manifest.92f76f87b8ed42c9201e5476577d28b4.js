819bac927deacfe63f4c29093071fe5b02f3cf29
Roman <roman@MacBook-Pro-Roman.local>
2020-08-16 21:47:15 +0300

    Updates

---
+++ precache-manifest.92f76f87b8ed42c9201e5476577d28b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f38114db88ae7f5dbcec81507948f3",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "97b9b9085058e1c0456d",
+    "url": "/todo-react/static/css/main.1ba88c8c.chunk.css"
+  },
+  {
+    "revision": "3f75d2814caea73d39fd",
+    "url": "/todo-react/static/js/2.25bec424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.25bec424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b9b9085058e1c0456d",
+    "url": "/todo-react/static/js/main.f2b3881c.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
